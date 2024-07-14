6692ddb7d1d524cb87d5052882a911449f27566e
momoko <momoko.yamada@woven.toyota>
2024-07-15 07:13:22 +0900

    Submit for granding

--- 04_sql_and_python/02_challenge/queries.py
+++ 04_sql_and_python/02_challenge/queries.py
@@ -13,7 +13,11 @@ def query_orders(db):
     戻り値:
         注文を表すタプルのリスト
     """
-    query = ""  # ここにSQLクエリを書いてください
+    query = """
+    SELECT *
+    FROM Orders
+    ORDER BY OrderID ASC
+    """ # ここにSQLクエリを書いてください
     results = db.execute(query)
     results = results.fetchall()
     return results
@@ -33,7 +37,12 @@ def get_orders_range(db, date_from, date_to):
     戻り値:
         注文を表すタプルのリスト
     """
-    query = ""  # ここにSQLクエリを書いてください
+    query = """
+    SELECT *
+    FROM Orders
+    WHERE OrderDate > '{date_from}' AND OrderDate <= '{date_to}'
+    ORDER BY OrderDate ASC
+    """ # ここにSQLクエリを書いてください
     results = db.execute(query)
     results = results.fetchall()
     return results
@@ -50,14 +59,19 @@ def get_order_details(db):
     戻り値:
         注文の詳細情報を表すタプルのリスト
     """
-    query = ""  # ここにSQLクエリを書いてください
+    query = """
+    SELECT OrderDetails.OrderID, Products.ProductName, OrderDetails.Quantity
+    FROM OrderDetails
+    INNER JOIN Products ON OrderDetails.ProductID = Products.ProductID
+    ORDER BY OrderDetails.OrderID ASC
+    """ # ここにSQLクエリを書いてください
     results = db.execute(query)
     results = results.fetchall()
     return results
 
 def main():
     # SQLiteデータベースに接続
-    conn = sqlite3.connect('northwind.db')
+    conn = sqlite3.connect('../data/northwind.db')
 
     # query_orders関数のテスト
     print("All Orders:")
